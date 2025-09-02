e4cf8f7bee46b758d83bec9e920e1ad9781fa1e5
sahas0412 <sahasabishek1@gmail.com>
2025-09-02 13:01:30 +0530

    initial commit

---
+++ 1st and last digits.py
@@ -0,0 +1,6 @@
+x=str(input("enter your number ="))
+digits = []
+for i in x:
+    digits.append(int(i))
+print("first number",digits[0])
+print("last number",digits[-1])
---
+++ count digits.py
@@ -0,0 +1,5 @@
+x=str(input("enter your number ="))
+p = 1
+for i in range(1,len(x)):
+    p += 1
+print(p)
